66a416d41cd2847a11725a3f5821891e140a9ef8
Zvan, Ben <ben.zvan@capella.edu>
2016-07-25 16:35:55 -0500

    Initial commit of starting framework.

---
+++ README.md
@@ -0,0 +1,1 @@
+A project to generate tech conference titles from lists of nouns, adjectives, and verbs.
---
+++ controller.php
@@ -0,0 +1,12 @@
+<?php
+$listOfNouns = "nouns.txt";
+$listOfVerbs = "verbs.txt";
+$listOfAdjectives = "adjectives.txt";
+$listOfTitleFormats = "titleFormats.txt";
+
+$URI = $_SERVER['REQUEST_URI'];
+
+print $URI;
+
+?>
+
