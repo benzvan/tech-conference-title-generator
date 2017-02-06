363ba9d93d18c7802ea27ed96f5cf6fe960dfba4
Ben Zvan <benjamin.zvan@target.com>
2017-02-06 09:46:58 -0600

    Moved content to content directory

---
+++ content/index.php
@@ -0,0 +1,82 @@
+<?php
+
+$dataPath = "data";
+
+$listOfFiles = [
+  "noun" => $dataPath . "/" . "nouns.txt",
+  "verb" => $dataPath . "/" . "verbs.txt",
+  "adjective" => $dataPath . "/" . "adjectives.txt",
+  "addonPhrase" => $dataPath . "/" . "addonPhrases.txt",
+  "titleFormat" => $dataPath . "/" . "titleFormats.txt",
+];
+
+$cwd = dirname(__FILE__);
+
+$noQuery = split("\?",$_SERVER['REQUEST_URI'])[0];
+//$URI = split("/",$_SERVER['REQUEST_URI']);
+$URI = split("/",$noQuery);
+
+
+
+function getRandomString($stringType, $stringModifier ) {
+  global $listOfFiles;
+  global $cwd;
+  if ( $listOfFiles[$stringType] != "" ) {
+    #print "string type = $stringType\n";
+    #print "file to read = $listOfFiles[$stringType]\n";
+    $fileContents = file( "file://$cwd/$listOfFiles[$stringType]");
+    $lineContents = split("@@",$fileContents[mt_rand(0, count($fileContents) - 1)]);
+    
+    if ( "$stringType" == "noun" ) {
+      if ( "$stringModifier" == "plural" ) {
+        if ( count($lineContents) == 2 ) {
+    	  return trim($lineContents[1]);
+        } else {
+          return trim($lineContents[0]) . "s";
+        }
+      } else {
+        return trim($lineContents[0]);
+      }
+
+    } else if ( "$stringType" == "adjective" ) {
+      return trim($lineContents[0]);
+
+    } else if ( "$stringType" == "verb" ) {
+      if ( "$stringModifier" == "present" ) {
+        return trim($lineContents[1]);
+      } else if ( "$stringModifier" == "past" ) {
+        return trim($lineContents[2]);
+      } else {
+        return trim($lineContents[0]);
+      }
+
+    } else if ( "$stringType" == "addonPhrase" ) {
+      return trim($lineContents[0]);
+
+    } else if ( "$stringType" == "titleFormat" ) {
+      return trim($lineContents[0]);
+
+    } else {
+      return "unrecognized getString type";
+    }
+
+  } else {
+    return "No";
+  }
+}
+
+if ( $URI[1] == "get"  ) {
+// Return json
+  if ( count($URI) == 4 ) {
+    $result = json_encode(getRandomString($URI[2],$URI[3]));
+  } else if ( count($URI) == 3 ) {
+    $result = json_encode(getRandomString($URI[2],""));
+  }
+  print "{\"string\":$result}";
+} else {
+// Display page
+  include "file://$cwd/mainpage.html";
+}
+
+?>
+
---
+++ content/scripts/builder.js
@@ -0,0 +1,53 @@
+var responseMessages = [ "request not initialized","server connection established","request received","processing request","request finished and response is ready" ];
+var urlsForThings = { "noun":"noun","pluralnoun":"noun/plural","adjective":"adjective","adjectivemore":"adjective/more","verb":"verb","presentcontinuousverb":"verb/present","addonPhrase":"addonPhrase","titleFormat":"titleFormat" };
+
+function getJSON(thingToGet, elementID, signal) {
+	if (typeof(signal)==='undefined') signal = false;
+        var xmlhttp = new XMLHttpRequest();
+        var jsonurl = "/get/";
+	var result = "";
+
+        xmlhttp.open("GET", jsonurl + urlsForThings[thingToGet] + "?" +  Math.random().toString().split("\.")[1] );
+        xmlhttp.send();
+
+        xmlhttp.onreadystatechange = function() {
+                var xmlhttpresult = "Current JSON request status: " + responseMessages[xmlhttp.readyState];
+                if (xmlhttp.readyState == 4) {
+                        if (xmlhttp.status == 200) {
+				// Success!!!
+				result = JSON.parse(xmlhttp.responseText).string;
+				document.getElementById(elementID).innerHTML = result;
+				if ( signal ) {
+					titleSet();
+				}
+                        } else { 
+				// Failure!!!
+                                xmlhttpresult += " with http status: " + xmlhttp.status;
+                        }
+                } else {
+			// Not done yet
+                }
+        }
+}
+
+function setTitleFormat() {
+	getJSON("titleFormat","conferencetitle",true);
+}
+
+function setTitleValue(itemID) {
+	var itemType = itemID.replace( /\d+$/, '' );
+	getJSON(itemType, itemID);
+}
+
+function fillInTitleBits() {
+	var titleChildren = document.getElementById("conferencetitle").children;	
+	for (i = 0; i < titleChildren.length; i++) {
+		setTitleValue(titleChildren[i].id);
+	}
+}
+
+function titleSet() {
+	fillInTitleBits();
+}
+
+setTitleFormat();
