b18cd151a0b3a7b72ca67b79fecde31f4b156f1d
Ben Zvan <benjamin.zvan@target.com>
2017-02-06 09:47:15 -0600

    Updated deploy script to be smarter (copied from chinatown)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
-TARGET=$1
-cp -r * "${TARGET}"
-rm "${TARGET}"/$0
+
+SOURCE=./content
+BASE=/var/www/hosts
+TEST=conferencetitle.zvan.net-beta
+PROD=conferencetitle.zvan.net
+
+git pull --rebase
+UPDATE="$(git log | head -1)"
+echo "Update version:  ${UPDATE}"
+
+if [ "$1" == pr ]; then
+  echo "Production!"
+  rsync -av "${SOURCE}"/ "${BASE}"/"${PROD}"/
+else
+  echo "Testing!"
+  echo "For production use: '$0 pr'"
+  rsync -av "${SOURCE}"/ "${BASE}"/"${TEST}"/
+fi
